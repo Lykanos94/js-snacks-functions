bd1788c97258cc256003ceb62c00d72b5508d3a0
Lykanos94 <luca77.94@libero.it>
2024-11-21 15:16:57 +0100

    base: third exercise completed

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -4,10 +4,23 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function findSpecificVowels(str) {
+    const specificVowels = 'ai'; 
+    let foundVowels = [];
 
+    for (let char of str) {
+        if (specificVowels.includes(char)) {
+            foundVowels.push(char);
+        }
+    }
+
+    return foundVowels; 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const Letter = 'javascript';
+const result = findSpecificVowels(word);
+console.log(result);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
